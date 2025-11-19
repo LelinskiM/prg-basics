fbd6c404801e0455ceea5e1ccc43932e2b61c9b1
Marcel <mlelinski2@gmail.com>
2025-11-19 15:49:57 +0100

    finnished secction 5?

--- 04-Functions/converters.py
+++ 04-Functions/converters.py
@@ -18,4 +18,7 @@ if __name__ == "__main__":
     # so you can test the functions in this place
     print(f'2m = {m_to_cm(2)}cm')
     print(f'532cm = {cm_to_m(532)}m')
+    print(f"12inch = {inch_to_cm(12)}cm')")
+    print(f'100cm = {cm_to_inch(100)}inch')
+    print(f'5feet = {feet_to_cm(5)}cm')
 
--- 04-Functions/test_converters.py
+++ 04-Functions/test_converters.py
@@ -2,5 +2,8 @@ import converters
 
 print('### Test converters')
 print(f'Three meters is {converters.m_to_cm(3)}cm')
+print(f"12inch = {converters.inch_to_cm(12)}cm')")
+print(f'100cm = {converters.cm_to_inch(100)}inch')
+print(f'5feet = {converters.feet_to_cm(5)}cm')
 
 
